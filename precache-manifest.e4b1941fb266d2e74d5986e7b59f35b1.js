a9d2427bfbffd37c16849f7b184dcb679b9bdfdf
Aatmik Mallya <aatmik54@gmail.com>
2020-07-22 03:04:44 -0700

    Updates

---
+++ precache-manifest.e4b1941fb266d2e74d5986e7b59f35b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f629633c8471e2e6885efeff7e3ddfd8",
+    "url": "/Sorting-Time-Visualizer/index.html"
+  },
+  {
+    "revision": "3adc8dab48cd86032956",
+    "url": "/Sorting-Time-Visualizer/static/css/main.80d5a33b.chunk.css"
+  },
+  {
+    "revision": "2a81ec7a09841b914d11",
+    "url": "/Sorting-Time-Visualizer/static/js/2.9866e936.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Sorting-Time-Visualizer/static/js/2.9866e936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3adc8dab48cd86032956",
+    "url": "/Sorting-Time-Visualizer/static/js/main.28113ae9.chunk.js"
+  },
+  {
+    "revision": "922ea9d18f6a6f827534",
+    "url": "/Sorting-Time-Visualizer/static/js/runtime-main.fa383a73.js"
+  },
+  {
+    "revision": "a45353c2cb06fd1231a8867e389d6055",
+    "url": "/Sorting-Time-Visualizer/static/media/card-flip.a45353c2.wav"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sorting-Time-Visualizer/precache-manifest.0f83f60c7c88ddbd63589a4ba749b8d9.js"
+  "/Sorting-Time-Visualizer/precache-manifest.e4b1941fb266d2e74d5986e7b59f35b1.js"
 );
 
 self.addEventListener('message', (event) => {

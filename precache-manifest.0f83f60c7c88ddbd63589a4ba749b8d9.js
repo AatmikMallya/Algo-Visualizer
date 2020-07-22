88150fc06c0233502c45e22d502af098e692c5d1
Aatmik Mallya <aatmik54@gmail.com>
2020-07-21 23:40:01 -0700

    Updates

--- precache-manifest.0f83f60c7c88ddbd63589a4ba749b8d9.js
+++ precache-manifest.0f83f60c7c88ddbd63589a4ba749b8d9.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "34cb5f6c0eaa54fdf9f08df817ae6da8",
+    "revision": "dbecf5bad9718d108c625c37b3e676ae",
     "url": "/Sorting-Time-Visualizer/index.html"
   },
   {
-    "revision": "8035cccc3bd37e3bd392",
-    "url": "/Sorting-Time-Visualizer/static/css/main.3db69a51.chunk.css"
+    "revision": "68369e6a1c73dfa9dbf5",
+    "url": "/Sorting-Time-Visualizer/static/css/main.3d1ecb78.chunk.css"
   },
   {
     "revision": "4d41534ca2f72d0b6662",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Sorting-Time-Visualizer/static/js/2.1026b29e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8035cccc3bd37e3bd392",
+    "revision": "68369e6a1c73dfa9dbf5",
     "url": "/Sorting-Time-Visualizer/static/js/main.d59dd657.chunk.js"
   },
   {

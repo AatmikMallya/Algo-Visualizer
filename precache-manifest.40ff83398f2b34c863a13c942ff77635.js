8b5f29fc83a32cb373c323caaa3dcff724d64041
Aatmik Mallya <aatmik54@gmail.com>
2020-07-29 22:56:46 -0700

    Updates

--- precache-manifest.40ff83398f2b34c863a13c942ff77635.js
+++ precache-manifest.40ff83398f2b34c863a13c942ff77635.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8a0a1cafb06fceadbd121c1e7292e398",
+    "revision": "9d8d20edb6c5d25dfb7bc7eed3a60546",
     "url": "/Sorting-Time-Visualizer/index.html"
   },
   {
-    "revision": "6a3e8231ff7d8ebcfe6f",
-    "url": "/Sorting-Time-Visualizer/static/css/main.ccb51bc2.chunk.css"
+    "revision": "bc0dcf16b786320ba542",
+    "url": "/Sorting-Time-Visualizer/static/css/main.17345232.chunk.css"
   },
   {
-    "revision": "e66002c59d0a727eda9d",
-    "url": "/Sorting-Time-Visualizer/static/js/2.00132c14.chunk.js"
+    "revision": "8f010faa26afcc7fef12",
+    "url": "/Sorting-Time-Visualizer/static/js/2.03065194.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/Sorting-Time-Visualizer/static/js/2.00132c14.chunk.js.LICENSE.txt"
+    "url": "/Sorting-Time-Visualizer/static/js/2.03065194.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6a3e8231ff7d8ebcfe6f",
-    "url": "/Sorting-Time-Visualizer/static/js/main.1e7b8b08.chunk.js"
+    "revision": "bc0dcf16b786320ba542",
+    "url": "/Sorting-Time-Visualizer/static/js/main.506c4598.chunk.js"
   },
   {
     "revision": "922ea9d18f6a6f827534",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Sorting-Time-Visualizer/precache-manifest.cdf0a83ab02b2a6ca7133e28b20732d3.js"
+  "/Sorting-Time-Visualizer/precache-manifest.40ff83398f2b34c863a13c942ff77635.js"
 );
 
 self.addEventListener('message', (event) => {

6c50141b004218d92ec8b4389ab14be5a2b36015
Aatmik Mallya <aatmik54@gmail.com>
2020-08-07 22:16:02 -0700

    change Google Analytics account

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ReactGA from 'react-ga'; // https://github.com/react-ga/react-ga
 function App() {
   // Google Analytics
   useEffect(() => {
-    ReactGA.initialize('UA-171800090-2');
+    ReactGA.initialize('UA-174975160-2');
     ReactGA.pageview(window.location.pathname);
   }, []);
 
